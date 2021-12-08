306760bcbbbe89e7ffa696eb3786915ba13fd6f4
MFigueroa-A <91798489+MFigueroa-A@users.noreply.github.com>
2021-12-08 13:44:02 -0300

    eliminacion de archivos

--- dbconfig.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    user: 'Administrador',
-    password: 'gt1Jw3fZEn',
-    server: 'projectangular-server.database.windows.net',
-    database: 'projectangular-db'
-}
-
-module.exports = config;
