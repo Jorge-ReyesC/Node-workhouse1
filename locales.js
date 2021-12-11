44bb2ae48f1adfa51ccdf41461e38d27036a1710
MFigueroa-A <91798489+MFigueroa-A@users.noreply.github.com>
2021-12-11 19:22:58 -0300

    Update locales.js

--- locales.js
+++ locales.js
@@ -1,11 +1,12 @@
 const config = require("./dbconfig")
 
 class locales{
-    constructor(id,nombre, direccion, region){
+    constructor(id,nombre, direccion, region, local1){
         this.id=id;
         this.nombre=nombre;
         this.direccion=direccion;
         this.region=region;
+        this.local1=local1;
     }
 }
 
